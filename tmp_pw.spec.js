988782116326825484da161925f5b5183a37bb81
ghw307261305 <ghw307261305@gmail.com>
2026-02-26 14:18:00 +0900

    delete temp

--- tmp_pw.spec.js
+++
@@ -1,7 +0,0 @@
-const { test } = require('@playwright/test');
-
-test('smoke', async ({ page }) => {
-  await page.goto('https://help.salesforce.com/s/articleView?id=release-notes.rn_einstein_copilot.htm&language=en_US&type=5&release=252');
-  await page.waitForTimeout(3000);
-  console.log('TITLE=', await page.title());
-});
